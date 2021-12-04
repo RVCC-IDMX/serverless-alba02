28ff0ab88a2841a5fafaa31bf5f0eb8ce3c6493a
alba02 <naomimaldo02@gmail.com>
2021-12-04 01:00:49 -0500

    Add main.js content

---
+++ dist/js/main.js
@@ -0,0 +1,30 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable indent */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable linebreak-style */
+const getRandomDadJoke = async () => {
+    const url = 'https://icanhazdadjoke.com/';
+    const jokeStream = await fetch(url, {
+        headers: {
+            Accept: 'application/json',
+        },
+    });
+    const jsonJoke = await jokeStream.json();
+    const joke = jsonJoke.joke;
+    return joke;
+};
+
+const displayJoke = (joke) => {
+  const h1 = document.querySelector('h1');
+  h1.textContent = joke;
+};
+
+const refreshJoke = async () => {
+  const joke = await getRandomDadJoke();
+  displayJoke(joke);
+};
+
+// load 1st joke
+refreshJoke();
+
+setInterval(refreshJoke, 10000);
